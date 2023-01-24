7b14ee6f6ccc557b2c1e9de5cd65f1f5118a6fef
unknown <renancavalcantercb@protonmail.com>
2023-01-24 01:10:16 -0300

    Added .gitignore

---
+++ application/README.md
@@ -0,0 +1,20 @@
+# Gepac Website
+### To do List
+
+- [x] Login Page
+- [x] II Semana da Física Register Page
+- [ ] BD in SQLite
+- [ ] Post Register Page
+- [ ] Post Login Page
+- [ ] Copyright Footer
+- [X] Like System
+- [X] View System
+- [ ] News API
+- [ ] Production
+- [ ] Astro Gallery
+
+
+### Right things to change
+- [ ] margin-bottom in card 2 (news.html)
+- [ ] https://freefrontend.com/bootstrap-galleries/  Galeria moonnight https://www.flickr.com/people/146020129@N04/
+- [ ] Download and added the Like and Views System
---
+++ application/static/js/semanadafisica.js
@@ -0,0 +1,49 @@
+function ValidaCPF() {
+    var RegraValida = document.getElementById("RegraValida").value;
+    var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
+    if (cpfValido.test(RegraValida) === true) {
+        console.log("CPF Válido");
+    } else {
+        console.log("CPF Inválido");
+    }
+}
+
+function fMasc(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascEx()", 1)
+}
+
+function fMascEx() {
+    obj.value = masc(obj.value)
+}
+
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
+
+function telMasc(objeto, mascara) {
+    obje = objeto
+    masca = mascara
+    setTimeout("telefoneMascEx()", 1)
+}
+
+function telefoneMascEx() {
+    obje.value = masca(obje.value)
+}
+
+function mTEL(tel) {
+    tel = tel.replace(/\D/g, "")
+    tel = tel.replace(/(\d{2})(\d)/, "($1)$2")
+    tel = tel.replace(/(\d{5})(\d)/, "$1$2")
+    tel = tel.replace(/(\d{4})(\d)/, "$1$2-")
+    return tel
+}
+
+function redirect() {
+    window.location.assign("semanadafisica.html")
+}
